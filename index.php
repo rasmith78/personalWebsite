58133c5b471de7ba189597f452497f141d0807e7
rasmith78 <roselynsmith78@gmail.com>
2016-11-03 12:44:43 -0600

    added index profile contact

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require_once("lib/headUtils.php");?>
+  </head>
+  <body>
+
+    <?php require_once("lib/partials/navbar.php");?>
+
+      <canvas id="canvas"></canvas>
+
+
+      <div class="jumbotron">
+          <h1 id="headText">Roselyn</h1>
+          <p id="headP">Farmington, New Mexico</p>
+        </div>
+
+
+
+    <footer>
+      <div class="container">
+        <a ref="#">
+           <p>
+            Roselyn Smith &copy; 2016.&middot;<a href="#">privacy</a>&middot;<a href="#">terms</a>
+          </p>
+      </div>
+    </footer>
+
+    <script type="text/javascript" src="lib/js/background.js"></script>
+
+  </body>
+</html>
---
+++ profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require_once("lib/headUtils.php");?>
+  </head>
+
+  <body>
+    <?php require_once("lib/partials/navbar.php");?>
+
+    <section>
+      
+            <img src="http://tinyurl.com/zstzqv5"</img src>
+          </div>
+    </section>
+
+  </body>
+</html>
